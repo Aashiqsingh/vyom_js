fe03b751e973f866cb632d19d25866d72575f526
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-06-23 18:25:03 +0530

    input

---
+++ Operator/operator.js
@@ -0,0 +1,20 @@
+var a = 25;
+var b = 25.00;
+
+
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
+
+
+
+
+// console.log(a + b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// console.log(4 ** 3);
+// console.log(3 ** 4);
+
---
+++ input/input.js
@@ -0,0 +1,12 @@
+// alert("Hello good evening...")
+
+// let x = confirm("Are you single?")
+// console.log(x);
+
+
+// let name = prompt("Enter your name :")
+// console.log(name);
+// console.log(typeof name);
+
+
+
