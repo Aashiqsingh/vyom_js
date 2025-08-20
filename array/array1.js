e604501aa014f928e3a7dc6c2d6ec12b987474ae
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-20 18:49:44 +0530

    push , pop

---
+++ array/array1.js
@@ -0,0 +1,35 @@
+// var arr = ["ram",12,56.67,true]
+// var users = ["vyom","priya","hiya","gandhi","singh"];
+// console.log(users);
+
+// console.log(users.length);
+
+// console.log(users[0]);
+// console.log(users[1]);
+
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].toUpperCase());
+// }
+
+// users.push("aashiq")
+// users.push("aashiq","aashiq","aashiq")
+
+// console.log(users);
+
+// users.pop()
+// console.log(users);
+
+// users.unshift("aashiq")
+// console.log(users);
+
+// users.shift()
+// console.log(users);
+
+var users = ["vyom","priya","hiya","gandhi","singh"];
+
+
+// users.splice(2,0,"aashiq")
+// users.splice(2,2)
+users.splice(2,2,"aashiq")
+console.log(users);
