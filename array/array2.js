049203217df624298d3d06e8196f3b286c911f55
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-29 19:14:59 +0530

    map

--- array/array2.js
+++ array/array2.js
@@ -17,3 +17,39 @@ let users = ["aman","aashiq","rahul","vijay","harshil","vyom"];
         
 //     }
 // })
+
+// map : it will return new array with same length
+
+// let ans = users.map((user)=>{
+//     return user.toUpperCase()
+// })
+
+// console.log(ans);
+
+
+// let ans = users.map((user)=>{
+//     return user.length > 5
+// })
+
+// console.log(ans);
+
+// filter : it will return new array with modified data
+
+
+// let ans = users.filter((user)=>{
+//     return user.length > 5
+// })
+
+// console.log(ans);
+
+// users.map((user)=>{
+//     console.log(user);
+    
+// })
+
+let newArr = [];
+users.forEach((user)=>{
+    newArr.push(user)
+})
+
+console.log(newArr);
